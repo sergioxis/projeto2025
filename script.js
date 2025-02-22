6f7e422c0a23b3ec2e09629c47b07cba11fc2b5a
sergioxis <sergiobrg06@gmail.com>
2025-02-22 08:09:01 -0300

    erro de digitação

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function togglemode() {
+function toggleMode() {
+ 
   const html = document.documentElement
+ 
   html.classList.toggle('light')
 
   const img = document.querySelector('#profile img')
    
-  if (html.classList.contains("light")) {
+  if(html.classList.contains('light')) {
 
     img.setAttribute('src', './assets/assets/avatar-light.png')
     
